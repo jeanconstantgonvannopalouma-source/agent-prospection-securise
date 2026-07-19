3efba9ef894cb5bc8f8a87fbac621a6ebef03352
Jean <jeanconstantgonvannopalouma@gmail.com>
2026-07-19 23:30:16 +0200

    ok

--- src/main.py
+++ src/main.py
@@ -106,7 +106,7 @@ def root():
 
 @app.route("/ui", methods=["GET"])
 def ui():
-    return render_template("ui.html")
+    return render_template("templates/ui.html")
 
 @app.route("/health", methods=["GET"])
 def health():
